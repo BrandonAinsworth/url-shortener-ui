20648f7b1abc3362394c964acaabaa5e9e4b82b3
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-08-09 09:30:22 -0600

    Add functionality to get all urls on app load, research codebase

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,8 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+
+export const postUrls = () => {
+  return fetch('http://localhost:3001/api/v1/urls')
+}
